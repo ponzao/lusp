6535f8378a9f2823705507f517e0c2590b555ad3
Vesa Marttila <vtmarttila@gmail.com>
2011-11-25 00:03:44 +0200

    " -> '

--- lusp.lua
+++ lusp.lua
@@ -6,7 +6,7 @@ env['true'] = true
 env['false'] = false
 env['nil'] = nil
 
-env["+"] = function(...)
+env['+'] = function(...)
   local sum = 0
   for _, v in pairs({...}) do
     sum = sum + v
@@ -14,7 +14,7 @@ env["+"] = function(...)
   return sum
 end
 
-env["-"] = function(...)
+env['-'] = function(...)
   local rv = nil
   for _, v in pairs({...}) do
     if not rv then
@@ -38,18 +38,18 @@ end
 
 function read_from(tokens)
   if #tokens == 0 then
-    error("Unexpected EOF while reading.")
+    error('Unexpected EOF while reading.')
   end
   local token = pop(tokens)
-  if "(" == token then
+  if '(' == token then
     local l = {}
-    while tokens[1] ~= ")" do
+    while tokens[1] ~= ')' do
       l[#l + 1] = read_from(tokens)
     end
     pop(tokens)
     return l
-  elseif ")" == token then
-    error("Unexpected )")
+  elseif ')' == token then
+    error('Unexpected )')
   else
     return atom(token)
   end
@@ -61,27 +61,27 @@ end
 
 function eval(x, local_env)
   if not local_env then local_env = env end
-  if isa(x, "string") then
+  if isa(x, 'string') then
     return find(local_env, x)[x]
-  elseif not isa(x, "table") then
+  elseif not isa(x, 'table') then
     return x
-  elseif x[1] == "quote" then
+  elseif x[1] == 'quote' then
     pop(x)
     return unpack(x)
-  elseif x[1] == "if" then
+  elseif x[1] == 'if' then
     local _ = pop(x)
     local test = pop(x)
     local exprs = pop(x)
     local alt_exprs = pop(x)
     return eval(eval(test, env) and exprs or alt_exprs, local_env)
-  elseif x[1] == "set!" then
-  elseif x[1] == "define" then
+  elseif x[1] == 'set!' then
+  elseif x[1] == 'define' then
     local _ = pop(x)
     local var = pop(x)
     local exp = pop(x)
     local_env[var] = eval(exp, local_env)
-  elseif x[1] == "lambda" then
-  elseif x[1] == "begin" then
+  elseif x[1] == 'lambda' then
+  elseif x[1] == 'begin' then
   else
     local exps = map(function(exp) return eval(exp, env) end, x)
     local proc = pop(exps)
@@ -102,9 +102,9 @@ function pop(t)
 end
 
 function tokenize(s)
-  local str = s:gsub("%(", " ( "):gsub("%)", " ) ")
+  local str = s:gsub('%(', ' ( '):gsub('%)', ' ) ')
   local tokens = {}
-  for word in str:gmatch("%S+") do
+  for word in str:gmatch('%S+') do
     tokens[#tokens + 1] = word
   end
   return tokens
@@ -117,8 +117,8 @@ end
 parse = read
 
 function to_string(exp)
-  return isa(exp, "table")
-           and "(" .. table.concat(map(to_string, exp), " ") .. ")"
+  return isa(exp, 'table')
+           and '(' .. table.concat(map(to_string, exp), ' ') .. ')'
            or  tostring(exp)
 end
 
